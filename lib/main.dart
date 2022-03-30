391cb1d5f53193e1a970e987334af56c772fecd7
swap-10 <swapnalv10@gmail.com>
2022-03-30 17:15:22 +0530

    Setup Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone_flutter/responsiveness/mobile_screen_layout.dart';
 import 'package:insta_clone_flutter/responsiveness/web_screen_layout.dart';
 import 'package:insta_clone_flutter/utils/colors.dart';
 import 'package:insta_clone_flutter/responsiveness/responsive_screen_layout.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: dotenv.env['apiKey']!,
+        appId: dotenv.env['appId']!,
+        messagingSenderId: dotenv.env['messagingSenderId']!,
+        projectId: dotenv.env['projectId']!,
+        storageBucket: dotenv.env['storageBucket']!,
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
