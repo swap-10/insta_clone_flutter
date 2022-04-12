d858fdac4af78d015eff2c15b3360081f8346a4c
swap-10 <swapnalv10@gmail.com>
2022-04-12 21:50:49 +0530

    Add DP select UI to signup screen; make signup screen more responsive

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:insta_clone_flutter/utils/colors.dart';
@@ -14,9 +15,11 @@ class SignupScreen extends StatefulWidget {
 
 class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController _emailAddressController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
+
   final String _errorText = "Password doesn't match";
   String? _passedErrorText;
 
@@ -24,6 +27,7 @@ class _SignupScreenState extends State<SignupScreen> {
   void dispose() {
     super.dispose();
     _emailAddressController.dispose();
+    _usernameController.dispose();
     _passwordController.dispose();
     _confirmPasswordController.dispose();
   }
@@ -38,108 +42,154 @@ class _SignupScreenState extends State<SignupScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Flexible(child: Container(), flex: 1),
-              Image.asset(
-                'assets/instagram_logo.png',
-                height: 48.0,
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              SvgPicture.asset(
-                'assets/Instagram_logo_written.svg',
-                color: primaryColor,
-                height: 48.0,
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              CustomTextField(
-                textEditingController: _emailAddressController,
-                hintText: "E-mail",
-                keyboardType: TextInputType.emailAddress,
-                obscureText: false,
-                enableSuggestions: true,
-                autocorrect: false,
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              CustomTextField(
-                textEditingController: _passwordController,
-                hintText: "Password",
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-              ),
-              const SizedBox(
-                height: 24.0,
-              ),
-              ValueListenableBuilder(
-                valueListenable: _confirmPasswordController,
-                builder: (context, TextEditingValue value, _) {
-                  if (value == _passwordController.value) {
-                    _passedErrorText = null;
-                  } else {
-                    _passedErrorText = _errorText;
-                  }
-                  return CustomTextField(
-                    textEditingController: _confirmPasswordController,
-                    hintText: "Confirm Password",
-                    errorText: _passedErrorText,
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                  );
-                },
-              ),
-              TextButton(
-                onPressed: (() => {}),
-                child: Container(
-                  child: const Text(
-                    "SignUp",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  width: double.infinity,
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.symmetric(vertical: 8.0),
-                  decoration: const ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              Flexible(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const SizedBox(
+                      height: 5.0,
                     ),
-                    color: Colors.blue,
-                  ),
+                    SvgPicture.asset(
+                      'assets/Instagram_logo_written.svg',
+                      color: primaryColor,
+                      height: 48.0,
+                    ),
+                    // Upload profile picture - Circular widget
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Stack(
+                            fit: StackFit.loose,
+                            children: [
+                              Center(
+                                child: CircleAvatar(
+                                  radius: 32.0,
+                                  backgroundImage:
+                                      Image.asset('assets/usericon.png').image,
+                                ),
+                              ),
+                              Positioned(
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.add_a_photo_outlined),
+                                  color: Colors.blue,
+                                ),
+                                bottom: -10,
+                                left: 180,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
               Flexible(
-                child: Container(),
-                flex: 2,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    child: const Text("Have an account already?"),
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 8.0, horizontal: 1.0),
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      child: const Text(
-                        "Login instead",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
+                flex: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Email Address
+                    CustomTextField(
+                      textEditingController: _emailAddressController,
+                      hintText: "E-mail",
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: false,
+                      enableSuggestions: true,
+                      autocorrect: false,
+                    ),
+                    // Username
+                    CustomTextField(
+                      textEditingController: _usernameController,
+                      hintText: "Username",
+                      keyboardType: TextInputType.text,
+                      obscureText: false,
+                      enableSuggestions: true,
+                      autocorrect: false,
+                    ),
+                    // Password
+                    CustomTextField(
+                      textEditingController: _passwordController,
+                      hintText: "Password",
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                    ),
+                    // Confirm Password
+                    ValueListenableBuilder(
+                      valueListenable: _confirmPasswordController,
+                      builder: (context, TextEditingValue value, _) {
+                        if (value == _passwordController.value) {
+                          _passedErrorText = null;
+                        } else {
+                          _passedErrorText = _errorText;
+                        }
+                        return CustomTextField(
+                          textEditingController: _confirmPasswordController,
+                          hintText: "Confirm Password",
+                          errorText: _passedErrorText,
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                        );
+                      },
+                    ),
+                    // SignUp
+                    TextButton(
+                      onPressed: (() => {}),
+                      child: Container(
+                        child: const Text(
+                          "SignUp",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        width: double.infinity,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        decoration: const ShapeDecoration(
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15.0)),
+                          ),
                           color: Colors.blue,
                         ),
                       ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // Login Instead
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: const Text("Have an account already?"),
                       padding: const EdgeInsets.symmetric(
                           vertical: 8.0, horizontal: 1.0),
                     ),
-                  ),
-                ],
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        child: const Text(
+                          "Login instead",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 1.0),
+                      ),
+                    ),
+                  ],
+                ),
               )
             ],
           ),
