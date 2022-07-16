6a2fb76ced0e2164bf9b103b2062c9bd5e6604e2
swap-10 <swapnalv10@gmail.com>
2022-07-16 14:10:15 +0530

    Add bottom navigation bar to MobileScreenLayout

--- lib/responsiveness/mobile_screen_layout.dart
+++ lib/responsiveness/mobile_screen_layout.dart
@@ -1,8 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:insta_clone_flutter/models/user.dart' as UserModel;
-import 'package:insta_clone_flutter/providers/user_provider.dart';
+import 'package:insta_clone_flutter/utils/colors.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatefulWidget {
@@ -19,20 +18,62 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
 
   @override
   Widget build(BuildContext context) {
-    UserModel.UserInfo userInfo = Provider.of<UserProvider>(context).getUser;
-
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(userInfo.username),
+            const Text("Mobile screen"),
             TextButton(
               onPressed: signOutUser,
               child: const Text("Sign Out"),
             )
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: mobileBackgroundColor,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blue,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              // color: Colors.white,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              // color: Colors.white,
+            ),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_circle_outline_rounded,
+              // color: Colors.white,
+            ),
+            label: "Post",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_outline_rounded,
+              // color: Colors.white,
+            ),
+            label: "Notifications",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              // color: Colors.white,
+            ),
+            label: "Profile",
+          ),
+        ],
       ),
     );
   }
