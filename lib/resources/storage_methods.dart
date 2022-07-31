9b7d1ecc0481a3ea60dcb7476e61c0f6e3622ed8
swap-10 <swapnalv10@gmail.com>
2022-07-31 14:35:05 +0530

    Add support for when upload img is a post, to StorageMethods().uploadImageToStorage(), add metadata to specify image/jpeg

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -3,6 +3,10 @@ import 'dart:typed_data';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:insta_clone_flutter/screens/add_post_screen.dart';
+import 'package:uuid/uuid.dart';
+
+import '../models/post.dart';
 
 class StorageMethods {
   final FirebaseStorage _storage = FirebaseStorage.instance;
@@ -12,8 +16,13 @@ class StorageMethods {
       String childName, Uint8List file, bool isPost) async {
     Reference ref =
         _storage.ref().child(childName).child(_auth.currentUser!.uid);
+    if (isPost) {
+      String id = const Uuid().v1();
+      ref = ref.child(id);
+    }
 
-    UploadTask uploadTask = ref.putData(file);
+    UploadTask uploadTask =
+        ref.putData(file, SettableMetadata(contentType: 'image/jpeg'));
 
     TaskSnapshot snapshot = await uploadTask;
     String downloadURL = await snapshot.ref.getDownloadURL();
