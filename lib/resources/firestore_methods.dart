3ee0294f905f446ec5de628bf6abca48736dbdb2
swap-10 <swapnalv10@gmail.com>
2022-08-01 13:49:20 +0530

    Add updateUserPosts to firestore_methods updateUserPosts function updates the list of postIDs for a user. call this function from within uploadPost function to update postIDs when post is uploaded.

--- lib/resources/firestore_methods.dart
+++ lib/resources/firestore_methods.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:insta_clone_flutter/models/post.dart';
+import 'package:insta_clone_flutter/models/user.dart' as user_model;
 import 'package:insta_clone_flutter/resources/storage_methods.dart';
 import 'package:uuid/uuid.dart';
 
@@ -29,10 +30,36 @@ class FirestoreMethods {
       );
 
       _firestore.collection('posts').doc(postID).set(post.toJSON());
+      res = await updateUserPosts(uid, postID);
       res = "Success!";
     } catch (err) {
       res = err.toString();
     }
+
+    return res;
+  }
+
+  Future<String> updateUserPosts(
+    String uid,
+    String postID,
+  ) async {
+    String res = "Something went wrong";
+    try {
+      DocumentSnapshot docSnap =
+          await _firestore.collection('users').doc(uid).get();
+      user_model.UserInfo userInfo = user_model.UserInfo.fromSnap(docSnap);
+      List currentPostIDs = userInfo.postIDs;
+      currentPostIDs.add(postID);
+
+      _firestore
+          .collection('users')
+          .doc(uid)
+          .update({"postIDs": currentPostIDs});
+      res = "Success!";
+    } catch (err) {
+      res = err.toString();
+    }
+
     return res;
   }
 }
