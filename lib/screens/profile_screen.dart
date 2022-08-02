994a9b702eca696844f7de26e2c54ac45e0fb40f
swap-10 <swapnalv10@gmail.com>
2022-08-02 12:13:20 +0530

    Add Edit Profile button to Profile Screen Navigates to wip edit profile screen Currently shows user DP and image picker to change DP

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,14 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:insta_clone_flutter/models/user.dart' as user_model;
 import 'package:insta_clone_flutter/providers/user_provider.dart';
 import 'package:insta_clone_flutter/resources/auth_methods.dart';
 import 'package:insta_clone_flutter/responsiveness/mobile_screen_layout.dart';
 import 'package:insta_clone_flutter/screens/add_post_screen.dart';
 import 'package:insta_clone_flutter/utils/colors.dart';
+import 'package:insta_clone_flutter/utils/utils.dart';
 import 'package:provider/provider.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -183,6 +187,88 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   ),
                 ],
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              EditProfileScreen(userInfo: userInfo),
+                        ),
+                      ),
+                      icon: const Icon(Icons.edit),
+                      label: const Text("Edit Profile"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class EditProfileScreen extends StatefulWidget {
+  final user_model.UserInfo userInfo;
+  const EditProfileScreen({Key? key, required this.userInfo}) : super(key: key);
+
+  @override
+  State<EditProfileScreen> createState() => _EditProfileScreenState();
+}
+
+class _EditProfileScreenState extends State<EditProfileScreen> {
+  Uint8List? _image;
+  void selectImage() async {
+    Uint8List img = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = img;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: const Text("Edit Profile"),
+      ),
+      body: SizedBox.expand(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  child: ClipOval(
+                    child: _image == null
+                        ? Image.network(
+                            widget.userInfo.dpURL,
+                            width: 120,
+                            height: 100,
+                          )
+                        : Image.memory(_image!),
+                  ),
+                ),
+                Positioned(
+                  child: IconButton(
+                    icon: const Icon(Icons.camera_alt_outlined),
+                    onPressed: selectImage,
+                  ),
+                  bottom: -2.0,
+                  right: -10.0,
+                ),
+              ],
             ),
           ],
         ),
